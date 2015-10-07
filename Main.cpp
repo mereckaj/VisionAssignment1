893beaea4ba65ca703e727b4f7e8d337890998f6
Julius Mereckas <mereckaj@tcd.ie>
2015-10-07 14:52:53 +0000

    Uh oh

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,9 @@
 //
 // Created by mereckaj on 07/10/15.
 //
-
-int main(int argc, char* argv[]){
-
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(int argc, char** argv){
+    char * imageLocation = "/home/mereckaj/ClionProject/VisionAssignment1/Images";
 }
